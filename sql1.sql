8764f9847f42e854fc1929062d24f58db1f63010
Pratham Jaulkar <prathamkj21@gmail.com>
2025-08-31 19:09:07 +0530

    Update sql1.sql

--- sql1.sql
+++ sql1.sql
@@ -9,3 +9,10 @@ DROP DATABASE temp1;
 
 USE temp2;
 -- toh hum temp 2 me ab sari chize karengee 
+IMP Commands
+SELECT 
+    query_name,
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
